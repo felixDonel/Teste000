2f9e40a7e2924782c59cb0482eee5582790547c1
felixDonel <43584410+felixDonel@users.noreply.github.com>
2018-10-01 19:26:36 -0300

    m

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-setTimeout(ApresentarModal,5000);
+setTimeout(ApresentarModal,1000);
 function ApresentarModal(){
     var modal = document.querySelector(".modal");
     
-    if(modal == null)
+    if(modal != null)
     {
-       
-    }else{
         document.querySelector(".modal").style.display = "block";
 
-        document.querySelector(".modal a").addEventListener("click", function(){
+        document
+            .querySelector(".modal a")
+            .addEventListener("click", function(){
             document.querySelector(".modal").style.display = "none";
         });
     }
 }
 
 
-alert(document.forms["modal_form"]);
+if(document.forms["modal_form"]!= undefined){
+    //validação
+    var form = document.forms["modal_form"];
+    
+    form.addEventListener("submit", validarFormModal);
+}
+
+function validarFormModal(evt){
+    var form = document.forms["modal_form"];
+    var imputEmail= form.email;
+    var valorEmail = form.email.value;
+    
+    
+    var posicaoArroba = valorEmail.indexOf("@");
+    if(
+        valorEmail != "" &&
+        valorEmail.indexOf("@") > 3 && //o 3 é o numero de caracter antes do @
+        valorEmail.lastIndexOf(".") > posicaoArroba
+    ){
+        alert("E-mail valido");
+    }else{
+        alert("E-mail invalido");
+        
+        evt.preventDefault();
+    }
+    valorEmail;
